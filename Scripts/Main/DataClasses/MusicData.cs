e38271cdf8ce9124eb8a2b85c3e7af76c7a15bb5
UnofficialAndrew <work.andrewviersen@gmail.com>
2024-08-13 02:01:58 -0400

    VidyaGameMakas save/load system and ystem management system. Excellent code taught me a lot about data driven OOP

---
+++ Scripts/Main/DataClasses/MusicData.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public static class MusicData {
+	
+	public static Dictionary<string, string> musicDic = new Dictionary<string, string>() {
+		{"res://Scenes/10_Main.tscn", "res://Audio/Music/" },
+        {"res://Scenes/20_Level1.tscn", "res://Audio/Music/" },
+		{"res://Scenes/30_Level2.tscn", "res://Audio/Music/" },
+	};
+
+}
