d20b2ef6432b11634f4f4fbb635d843050f179fb
UnofficialAndrew <work.andrewviersen@gmail.com>
2024-11-15 21:56:03 -0500

    added the structure for items in player inventory + quest completion

--- Scripts/Main/DataClasses/Quest.cs
+++ Scripts/Main/DataClasses/Quest.cs
@@ -10,9 +10,16 @@ public class Quest: Node
     public string QuestDescription = { get; set; }
     public int CurrentStage = { get; set; }
     public bool QuestActive = { get; set; }
+    // Reference to GameStateFlags for world triggers
+    private GameStateFlags gameStateFlags;
     // Stages: Dicitionary that maps the quest's stage together.
     public Dicitionary<string, Dicitionary<string, object>> Stages {get; set; }
     
+    public override void _Ready()
+    {
+        gameStateFlags = GetNode<GameStateFlags>("res://Scripts/Main/GameStateFlags.cs")
+    }
+
     // Constructor to intialize properties
     public Quest(string questId, string questName, string questDescription)
     {
@@ -20,5 +27,25 @@ public class Quest: Node
         QuestName = questName;
         QuestDescription = questDescription;
         
+    }
+
+    public void CompleteQuest(int money, int renown, int experience, List<string> items, List<string> worldFlagsToTrigger)
+    {
+        // Update the player's money
+        GD.Print($"Awarding {money} gold to the player")
+        // Update the player's renown
+        GD.Print($"Awarding {renown} renown to the player")
+        // Update the player's experience points
+        GD.Print($"Awarding {experience} experience to the player")
+       
+
+        if (items != null && items.Count > 0)
+        {
+            foreach (string item in items)
+            {
+                GD.Print($"Awarding {items} renown to the player")
+            }
+        }
+
     }
 }
--- Scripts/Main/DataClasses/assests/PlayerLogic.cs
+++ Scripts/Main/DataClasses/assests/PlayerLogic.cs
@@ -4,10 +4,19 @@ using System.Security.Cryptography.X509Certificates;
 
 public partial class PlayerLogic : CharacterBody2D
 {
-	[Export]
+	[Export] // Allows you to export a value with the class member and becomes editing becomes available in the property editor
     public int Speed { get; set; } = 400;
 	[Export]
 	public int DashSpeed { get; set; } = 600;
+    [Export]
+    public int Health { get; set; } = 100;
+    [Export]
+    public int Money { get; set; } = 0;
+    [Export]
+    public int Experience { get; set; } = 0;
+    [Export]
+    public int Renown { get; set; } = 0;
+    public int[,,] Inventory = {{},{},{}}
     
     
 
